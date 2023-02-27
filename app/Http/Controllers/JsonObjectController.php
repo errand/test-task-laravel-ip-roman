8b1bb118dd56cf57871e0a5983f92a2b3315e904
Aleksandr Shatskih <work@errand.ru>
2023-02-27 16:17:49 +0700

    feat: added JsonObject Controller style: added Tailwind, vite.js

---
+++ app/Http/Controllers/JsonObjectController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JsonObject;
+use App\Http\Requests\StoreJsonObjectRequest;
+use App\Http\Requests\UpdateJsonObjectRequest;
+
+class JsonObjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreJsonObjectRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(JsonObject $jsonObject)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(JsonObject $jsonObject)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateJsonObjectRequest $request, JsonObject $jsonObject)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(JsonObject $jsonObject)
+    {
+        //
+    }
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    @yield('styles')
+
+    <title>@yield('title')</title>
+
+    @vite('resources/css/app.css')
+
+</head>
+<body>
+<div id="app">
+    <main class="py-4">
+        @yield('content')
+    </main>
+</div>
+@yield('footer')
+@vite('resources/js/app.js')
+</body>
+</html>
